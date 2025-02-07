c01050c05175920d7cb9a80223867203b852da4a
Santiago Cecena <santiago.cecena@encora.com>
2025-02-06 20:01:27 -0700

    feat: add search form and new todo button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import SearchForm from "./components/search-controls/SearchForm";
+import Button from "./components/UI/Button";
 
 function App() {
 
 	return (
-		<main className="p-6">
-			<h1 className="text-4xl text-center">To do list App</h1>
+		<main className="p-6 flex flex-col items-center">
+			<SearchForm />
+			<div className="w-full max-w-4xl mt-4">
+				<Button text="+ New To Do" classname="justify-self-start" onClick={() => { }} />
+			</div>
 		</main>
 	)
 }
