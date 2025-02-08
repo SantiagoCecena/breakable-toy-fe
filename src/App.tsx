4aba313e99b85dfda4c8c8dc08be51ff85bf7c8a
Santiago Cecena <santiago.cecena@encora.com>
2025-02-08 16:14:10 -0700

    feat: add TodosTable component to the main app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import SearchForm from "./components/search-controls/SearchForm";
+import TodosTable from "./components/todos-table/TodosTable";
 import Button from "./components/UI/Button";
 
 function App() {
 
 	return (
-		<main className="p-6 flex flex-col items-center">
+		<main className="p-6 pb-10 flex flex-col items-center">
 			<SearchForm />
 			<div className="w-full max-w-4xl mt-4">
 				<Button text="+ New To Do" classname="justify-self-start" onClick={() => { }} />
 			</div>
+			<TodosTable />
+			{/* //todo: add the todo's table here */}
 		</main>
 	)
 }
