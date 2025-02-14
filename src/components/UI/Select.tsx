8d2eebf274a9f5818b70a4fb5f404938fa615fc0
Santiago Cecena <santiago.cecena@encora.com>
2025-02-14 00:45:08 -0700

    feat: extend SelectProps to include standard HTML select attributes

--- src/components/UI/Select.tsx
+++ src/components/UI/Select.tsx
@@ -1,5 +1,5 @@
 
-interface SelectProps {
+interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
     label: string;
     children: React.ReactNode;
     name?: string;
@@ -7,12 +7,12 @@ interface SelectProps {
     classname?: string;
 }
 
-function Select({ label, children, id, name, classname }: SelectProps) {
+function Select({ label, children, id, name, classname, ...props }: SelectProps) {
 
     return (
         <div className="flex gap-x-2 items-center my-2">
             <label htmlFor={id} className="min-w-[55px]">{label}</label>
-            <select id={id} name={name} className={`p-2.5 border border-gray-300 ocus:outline focus:outline-1 focus:outline-gray-500 shadow-sm rounded-md w-full bg-transparent ${classname}`}>
+            <select id={id} name={name} className={`p-2.5 border border-gray-300 ocus:outline focus:outline-1 focus:outline-gray-500 shadow-sm rounded-md w-full bg-transparent ${classname}`} {...props}>
                 {children}
             </select>
         </div>
